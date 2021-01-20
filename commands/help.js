044392ed66b577a788970a1a4411051b9f4bac45
Parkeymon <68636315+Parkeymon@users.noreply.github.com>
2021-01-20 12:01:55 -0800

    added help command

---
+++ commands/help.js
@@ -0,0 +1,17 @@
+const { description } = require("./serverinfo")
+
+module.exports = {
+    name: 'help',
+    execute(message, args){
+        const helpEmbed = new Discord.MessageEmbed()
+        .setColor('#ff0000')
+        .setTitle('HELP!')
+        .setDescription('Help with the bot.')
+        .addField(
+            {name: 'ping', value: 'Pings the bot.'},
+            {name: 'members', value: 'Displays member count.'},
+            {name: 'poll <your poll>', value: 'Makes a poll.'},
+            {name: 'serverinfo', value: 'Shows info about the server.'}
+        )
+    }
+}
